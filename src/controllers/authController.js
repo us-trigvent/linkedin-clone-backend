4695d7c51c26de01fa823437e447bba43519c169
Umesh Sobti <25602umesh.2020@gmail.com>
2026-07-16 21:27:56 +0530

    changes

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -70,6 +70,7 @@ const signup = async (req, res) => {
 const login = async (req, res) => {
     try {
         const { email, password } = req.body;
+        
         if (!email || !password) {
             return res.status(400).json({
                 success: false,
