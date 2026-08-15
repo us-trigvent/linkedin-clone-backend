ba4986de81e9f3e0fa83acacf17bf9c7fcee5bf8
Umesh Sobti <25602umesh.2020@gmail.com>
2026-08-15 17:45:05 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -4,26 +4,11 @@ const cookieParser = require("cookie-parser");
 const app = express();
 const postRoutes = require("./routes/postRoutes");
 
-app.set("trust proxy", 1);
-
-const allowedOrigins = [
-  "http://localhost:5173",
-  process.env.FRONTEND_URL,
-].filter(Boolean);
-
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-        return;
-      }
-
-      callback(new Error("Not allowed by CORS"));
-    },
+    origin: "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 app.use(express.json());
