d4d273589d349309d4f32bf1f075550947af0c0c
us-trigvent <umesh@trigvent.com>
2026-09-04 16:25:15 +0530

    changes to user profile router

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middleware/authMiddleware");
-const {getUserProfile} = require("../controllers/userController");
-router.get("/:userId",authMiddleware,getUserProfile);
+const { getUserProfile } = require("../controllers/userController");
+router.get("/profile/:userId", authMiddleware, getUserProfile);
 module.exports = router;
